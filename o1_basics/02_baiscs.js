67db0856a48ef66bcd609219ef8d8a1060d0dbc3
AlishbaSamar <samaralishbasamar@gmail.com>
2025-09-11 11:20:21 +0000

    Add 02_baiscs.js file

---
+++ o1_basics/02_baiscs.js
@@ -0,0 +1,17 @@
+// "use strict"; //treat all JS code as newer version
+// alert( 3+3)  we are using node js not browser
+let name = "hitesh"
+let age = 20
+let isLoggedIn = false
+// number => 2 to power 53
+//bigint
+// string = ""
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+//symbol => unique
+//object
+console.log(typeof "Alishba")
+// git add o1_basics/02_baiscs.js
+// git commit -m "Add 02_baiscs.js file"
+// git push origin main
