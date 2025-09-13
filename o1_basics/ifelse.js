545b2002bf2484ae8dfa86fe70fab1546a70c246
AlishbaSamar <samaralishbasamar@gmail.com>
2025-09-13 11:21:55 +0000

    javascript ifelse

---
+++ o1_basics/ifelse.js
@@ -0,0 +1,17 @@
+// console.log(null>=0)
+// console.log("2"===2)
+// if(2>3){
+//     console.log("good")
+// }
+// else{
+//     console.log("not good")
+// }
+names = []//array
+let obj = {
+    name:"hitesh",
+    age:22
+}
+const myfunction = function(){
+    console.log("Hello WOrld")
+}
+console.log(typeof obj)
