62505b888357dd29ccd0391e21852f667e85dc3c
AlishbaSamar <samaralishbasamar@gmail.com>
2025-09-13 11:47:43 +0000

    STRINGS

--- o1_basics/ifelse.js
+++ o1_basics/ifelse.js
@@ -1,17 +1,22 @@
-// console.log(null>=0)
-// console.log("2"===2)
-// if(2>3){
-//     console.log("good")
+// // console.log(null>=0)
+// // console.log("2"===2)
+// // if(2>3){
+// //     console.log("good")
+// // }
+// // else{
+// //     console.log("not good")
+// // }
+// names = []//array
+// let obj = {
+//     name:"hitesh",
+//     age:22
 // }
-// else{
-//     console.log("not good")
+// const myfunction = function(){
+//     console.log("Hello WOrld")
 // }
-names = []//array
-let obj = {
-    name:"hitesh",
-    age:22
-}
-const myfunction = function(){
-    console.log("Hello WOrld")
-}
-console.log(typeof obj)
+// console.log(typeof obj)
+const names = "Alishba"
+const repo = 40
+console.log(`My name is ${names} repo is ${repo}`);
+
+
