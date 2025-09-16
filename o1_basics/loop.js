e42ce3ecafce388a664552c030f1dd4b2a673052
AlishbaSamar <samaralishbasamar@gmail.com>
2025-09-16 10:34:02 +0000

    for loop

---
+++ o1_basics/loop.js
@@ -0,0 +1,11 @@
+// for loop
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//    console.log(element)    
+// }
+// 
+let marray = ["ali","Ammar","Alishba","Abeera"]
+for (let index = 0; index < marray.length; index++) {
+    const element = marray[index];
+    console.log(element)
+}
