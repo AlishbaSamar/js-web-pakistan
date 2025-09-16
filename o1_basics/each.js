b8099237d77dc3c2ce9461608545d0215d30b875
AlishbaSamar <samaralishbasamar@gmail.com>
2025-09-16 11:15:32 +0000

    looping

---
+++ o1_basics/each.js
@@ -0,0 +1,4 @@
+const code = ["JS","C++","Java","Python"]
+code.forEach(function(val){
+    console.log(val)
+})
---
+++ o1_basics/while.js
@@ -0,0 +1,11 @@
+let array = ["1","2","3"]
+let arr = 0
+while(arr < array.length){
+    console.log(`Value is ${array[arr]}`)
+    arr = arr+1
+}
+let s = 1
+do{
+   console.log(`Score is ${s}`)
+   s++
+}while(s<=10)
